c6ce4175b8e42b239600d5fb3f7137e724c84057
Mark S. Bentley <mark@lunartech.org>
2020-10-19 14:30:44 +0200

    Created minimal package

---
+++ everlink/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+"""
+__all__ = ['everlink']
+
+from bepicolombo import *
+
+# Set up the root logger
+
+import logging
+import sys
+logging.basicConfig(format='%(levelname)s %(asctime)s (%(name)s): %(message)s',
+                    level=logging.INFO, stream=sys.stdout, datefmt='%Y-%m-%d %H:%M:%S')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='everlink',
+      version='0.1',
+      description='everlink - re-linking broken Evernote links',
+      url='https://github.com/msbentley/everlink',
+      author='Mark S. Bentley',
+      author_email='mark@lunartech.org',
+      packages=['everlink'],
+      zip_safe=False)
